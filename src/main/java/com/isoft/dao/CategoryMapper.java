dba201338db89a9f53fd89ca3b2ea9eb4960e023
黄涛 <1198787527@qq.com>
2021-02-25 20:49:21 +0800

    update phone

--- src/main/java/com/isoft/dao/CategoryMapper.java
+++ src/main/java/com/isoft/dao/CategoryMapper.java
@@ -4,13 +4,14 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.isoft.pojo.entity.Category;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.isoft.pojo.vo.CategoryVo;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
 
 
 public interface CategoryMapper extends BaseMapper<Category> {
 
-    Page<CategoryVo> selCateList(Page<CategoryVo> page);
+	Page<CategoryVo> selCateList(Page<CategoryVo> page);
 
-	@Update("update Category set creator = #{state} where id = #{id}")
-	int pullScenery(Integer id, Integer state);
+	@Update("update category set creator = #{state} where id = #{id}")
+	int pullScenery(@Param("id") Integer id, @Param("state") Integer state);
 }
